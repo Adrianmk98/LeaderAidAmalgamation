1caf42d2a06ba344006601cd5ad12cf0c9b4dcb3
Adrian <adrianmkrzyzanowski@gmail.com>
2025-02-21 21:59:56 -0500

    Main

---
+++ Main.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import activitycheckMain
+import voteanalyzerMain
+import commentreaderMain
+from tkinter import PhotoImage
+'''
+
+ Program Purpose: Front end of the application. Allows the user to navigate to the option they wish to use.
+ 
+ 
+ '''
+
+# Function to run the main function of each script
+def open_activitycheck():
+    activitycheckMain.main()
+
+def open_voteanalyzer():
+    voteanalyzerMain.main()
+
+def open_commentreader():
+    commentreaderMain.main()
+
+def open_playerupdater():
+    import playerUpdater
+    playerUpdater.playerUpdater()
+
+# Create the main window
+root = tk.Tk()
+root.title("Dashboard")
+root.geometry("300x200")
+logo = PhotoImage(file="logored.png")
+root.iconphoto(False, logo)
+
+# Create buttons
+btn_activitycheck = tk.Button(root, text="Open Activity Check", command=open_activitycheck)
+btn_activitycheck.pack(pady=10)
+
+btn_voteanalyzer = tk.Button(root, text="Open Vote Analyzer", command=open_voteanalyzer)
+btn_voteanalyzer.pack(pady=10)
+
+btn_commentreader = tk.Button(root, text="Open Comment Reader", command=open_commentreader)
+btn_commentreader.pack(pady=10)
+
+btn_playerupdater = tk.Button(root, text="Open Player Updater", command=open_playerupdater)
+btn_playerupdater.pack(pady=10)
+
+root.mainloop()
